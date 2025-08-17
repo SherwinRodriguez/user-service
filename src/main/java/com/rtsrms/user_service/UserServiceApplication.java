a6d80565bcd3d9637bd13c8d993da2fcbde7a931
SherwinRodriguez <sherwin7rodriguez10@gmail.com>
2025-08-17 20:57:44 +0530

    spring dependencies

--- src/main/java/com/rtsrms/user_service/UserServiceApplication.java
+++ src/main/java/com/rtsrms/user_service/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.user.user_service;
+package com.rtsrms.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/rtsrms/user_service/UserServiceApplicationTests.java
+++ src/test/java/com/rtsrms/user_service/UserServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.user.user_service;
+package com.rtsrms.user_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
